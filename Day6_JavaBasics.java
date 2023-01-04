5f2debb45ea2165e7215d57542ebc333f3f8cdfb
Ajay Kamble <kambleaju73@gmail.com>
2023-01-04 13:30:06 +0530

    update

---
+++ Day6_JavaBasics.java
@@ -0,0 +1,18 @@
+import java.util.jar.Attributes.Name;
+
+public class Day6_JavaBasics {
+    //  Global Variables 
+     String name ;
+     static String place="Mumbai";
+
+     public static void main(String[] args) {
+        System.out.println(place);
+        System.out.println(name);
+
+     }
+public void show() {
+    int age = 10; 
+    System.out.println(age);
+}
+
+}
